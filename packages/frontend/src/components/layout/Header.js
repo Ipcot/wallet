aee0be85184a37e34f0bbd0e786ec9f9ff9b1568
Tatianayarchenko <ktanya61@gmail.com>
2022-11-18 16:33:34 +0200

    add loader

--- packages/frontend/src/components/layout/Header.js
+++ packages/frontend/src/components/layout/Header.js
@@ -1,13 +1,23 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-  return <div style={{height: 80, background: 'violet'}}>
-    I'm header
+  return (
+    <div style={{ height: 80, background: 'violet' }}>
+      I'm header
+      <NavLink
+        style={{ display: 'inline-block', marginLeft: 600, padding: 20 }}
+        to={'home'}
+      >
+        Home
+      </NavLink>
+      <NavLink to={'auth/login'} style={{ padding: 20 }}>
+        Login
+      </NavLink>
+      <NavLink to={'auth/register'} style={{ padding: 20 }}>
+        Register
+      </NavLink>
+    </div>
+  );
+};
 
-    <NavLink style={{display: 'inline-block', marginLeft: 600, padding: 20}} to={'home'}>Home</NavLink>
-    <NavLink to={'auth/login'} style={{padding: 20}}>Login</NavLink>
-    <NavLink to={'auth/register'} style={{padding: 20}}>Register</NavLink>
-  </div>
-}
-
-export default Header
+export default Header;
--- packages/frontend/src/components/layout/Loader.js
+++
@@ -1,4 +0,0 @@
-const Loader = () => {
-  return <div>Loading...</div>
-}
-export default Loader
