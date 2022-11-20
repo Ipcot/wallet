fca1bef7613105a79949940d0593ae36f2da029a
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-11-20 22:26:03 +0200

    swagger startkit

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -2,29 +2,13 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
-const swaggerUI = require("swagger-ui-express");
-const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const usersRouter = require('./routes/api/users')
 const transactionsRouter = require('./routes/api/transactions')
 
-const options = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Wallet API",
-			version: "1.0.0",
-			description: "An Express Wallet API",
-		},
-	},
-	apis: ["./routes/api/*.js"],
-};
-
-const specs = swaggerJsDoc(options);
-
 const app = express()
-
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
@@ -35,6 +19,8 @@ app.use(express.static('public'));
 
 app.use('/api/users', usersRouter)
 app.use('/api/transactions', transactionsRouter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
