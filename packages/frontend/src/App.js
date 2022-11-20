ec31c1d32cf90e016f9831575bf378c14110a6a1
Tatianayarchenko <ktanya61@gmail.com>
2022-11-20 13:02:31 +0200

    moved loader files

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import Pages from './pages';
-import Loader from 'components/layout/loader/Loader';
+import Loader from 'components/layout/Loader';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App() {
