0e8c9b9b1b31d152797f99fcf0e68343994bf85a
PlatinumKellerman <platinumm@ukr.net>
2022-11-27 13:58:26 +0200

    add current user balance from store

--- packages/frontend/src/components/HomeAside/CurrentBalance/CurrentBalance.jsx
+++ packages/frontend/src/components/HomeAside/CurrentBalance/CurrentBalance.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import contactsSelectors from 'store/transactions/transactionsSelectors';
+import transactionsOperations from 'store/transactions/transactionsOperations';
 import {
   BalanceWrapper,
   BalanceTitle,
@@ -5,10 +9,23 @@ import {
 } from './CurrentBalance.styled';
 
 const CurrentBalance = () => {
+  let balance = 0;
+  const dispatch = useDispatch();
+  const allTransaction = useSelector(contactsSelectors.getAllTransactions);
+
+  useEffect(() => {
+    dispatch(transactionsOperations.fetchTransactions());
+  }, [dispatch]);
+
+  if (allTransaction?.length > 0) {
+    const lastTransaction = allTransaction[0];
+    balance = spaceCreator(lastTransaction.balance);
+  }
+
   return (
     <BalanceWrapper>
       <BalanceTitle>Your balance</BalanceTitle>
-      <BalanceValue>$24 000.00</BalanceValue>
+      <BalanceValue>&#8372; {balance}</BalanceValue>
     </BalanceWrapper>
   );
 };
