2f53c0fa6cd916e3174992cf891a93b6a76514aa
PlatinumKellerman <platinumm@ukr.net>
2022-11-29 19:46:18 +0200

    add logoButton(to /home), red balance value when current balance is negative, some icon fixes

--- packages/frontend/src/components/HomeAside/CurrentBalance/CurrentBalance.jsx
+++ packages/frontend/src/components/HomeAside/CurrentBalance/CurrentBalance.jsx
@@ -23,13 +23,22 @@ const CurrentBalance = () => {
     allTransaction.transactions.data.length > 0
   ) {
     const lastTransaction = allTransaction.transactions.data[0];
-    balance = lastTransaction.balance;
+    balance = lastTransaction.balance.toFixed(2);
   }
 
   return (
     <BalanceWrapper>
       <BalanceTitle>Your balance</BalanceTitle>
-      <BalanceValue>&#x20b4; {balance}</BalanceValue>
+      <BalanceValue>
+        &#x20b4;{' '}
+        <span
+          style={{
+            color: balance > 0 ? '#000000' : '#FF6596',
+          }}
+        >
+          {balance}
+        </span>
+      </BalanceValue>
     </BalanceWrapper>
   );
 };
